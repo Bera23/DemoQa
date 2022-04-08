ade31d7decce6962c02ac7f9e742d69d863465d0
Uros Beric <47391101+Bera23@users.noreply.github.com>
2022-04-08 13:40:39 +0200

    Add files via upload

---
+++ Tests/BaseTest.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using DemoQa.Driver;
+using DemoQa.Pages;
+using DemoQa.Pages.Alerts;
+using DemoQa.Pages.BookStore;
+using DemoQa.Pages.ElementPage;
+using DemoQa.Pages.Interactions;
+using DemoQa.Pages.PracticeForm;
+using DemoQa.Pages.Widgets;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+namespace DemoQa.Tests
+{
+    public class BaseTest : SwitchDriver
+    {
+        public const string BROWSER = "fireFox";
+        public const string URL = "https://demoqa.com/";
+        public const int TIMEOUT = 20;
+
+        public HomePage homePage;
+        public TextBox textBox;
+        public CheckBox checkBox;
+        public RadioButton radioButton;
+        public Webtables webtables;
+        public Buttons buttons;
+        public Links links;
+        public BrokenLinks brokenLinks;
+        public UploadDownload uploadDownload;
+        public DynamicProperties dynamicProperties;
+        public PracticeForm practiceForm;
+        public BrowserWindows browserWindows;
+        public Alerts alerts;
+        public ModalDialogs modalDialogs;
+        public Accordian accordian;
+        public AutoComplete autoComplete;
+        public DatePicker datePicker;
+        public Slider slider;
+        public ProgressBar progressBar;
+        public Tabs tabs;
+        public ToolTips toolTips;
+        public Menu menu;
+        public SelectMenu selectMenu;
+        public Sortable sortable;
+        public Selectable selectable;
+        public Resizable resizable;
+        public Droppable droppable;
+        public Dragabble dragabble;
+        public Login login;
+        public Register register;
+        public BookStore bookStore;
+        public Profile profile;
+
+        public WebDriverWait wait;
+        public Actions actions;
+
+        public void Scroll(int vertical, int horizontal)
+        {
+            IJavaScriptExecutor js = driver;
+            js.ExecuteScript($"window.scrollBy({horizontal},{vertical})");
+        }
+
+        public void Zoom(double zoom)
+        {
+            IJavaScriptExecutor executor = driver;
+            executor.ExecuteScript($"document.body.style.zoom = '{zoom}'");
+        }
+        public void SwitchTab(int tab)
+        {
+            driver.SwitchTo().Window(driver.WindowHandles[tab]);
+        }
+
+        public void GoBack()
+        {
+            driver.Navigate().Back();
+        }
+
+        public void PressAlert()
+        {
+            wait.Until(ExpectedConditions.AlertIsPresent());
+            driver.SwitchTo().Alert().Accept();
+        }
+        public void TextAlert(string alert)
+        {
+            wait.Until(ExpectedConditions.AlertIsPresent());
+            driver.SwitchTo().Alert().SendKeys(alert);
+        }
+        public void Refresh()
+        {
+            driver.Navigate().Refresh();
+        }
+        public void HideElements()
+        {
+            IJavaScriptExecutor executor = driver;
+            executor.ExecuteScript("arguments[0].style.visibility='hidden'",
+                wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id = 'app']//child::footer"))));
+        }
+        public void CloseAds()
+        {
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//*[@id='close-fixedban']"))).Click();
+        }
+
+        public void DefaultFrame()
+        {
+            driver.SwitchTo().DefaultContent();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            Switch(BROWSER);
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(TIMEOUT));
+
+            driver.Navigate().GoToUrl(URL);
+            driver.Manage().Window.Maximize();
+
+            actions = new Actions(driver);
+
+            homePage = new HomePage(driver, wait);
+            textBox = new TextBox(driver, wait);
+            checkBox = new CheckBox(driver, wait);
+            radioButton = new RadioButton(driver, wait);
+            webtables = new Webtables(driver, wait);
+            buttons = new Buttons(driver, wait, actions);
+            links = new Links(driver, wait);
+            brokenLinks = new BrokenLinks(driver, wait);
+            uploadDownload = new UploadDownload(driver, wait);
+            dynamicProperties = new DynamicProperties(driver, wait);
+            practiceForm = new PracticeForm(driver, wait);
+            browserWindows = new BrowserWindows(driver, wait);
+            alerts = new Alerts(driver, wait);
+            modalDialogs = new ModalDialogs(driver, wait);
+            accordian = new Accordian(driver, wait);
+            autoComplete = new AutoComplete(driver, wait);
+            datePicker = new DatePicker(driver, wait);
+            slider = new Slider(driver, wait);
+            progressBar = new ProgressBar(driver, wait);
+            tabs = new Tabs(driver, wait);
+            toolTips = new ToolTips(driver, wait);
+            menu = new Menu(driver, wait);
+            selectMenu = new SelectMenu(driver, wait, actions);
+            sortable = new Sortable(driver, wait, actions);
+            selectable = new Selectable(driver, wait);
+            resizable = new Resizable(driver, wait, actions);
+            droppable = new Droppable(driver, wait, actions);
+            dragabble = new Dragabble(driver, wait, actions);
+            login = new Login(driver, wait);
+            register = new Register(driver, wait);
+            bookStore = new BookStore(driver, wait);
+            profile = new Profile(driver, wait);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            //driver.Quit();
+        }
+    }
+}
---
+++ Tests/SampleTest.cs
@@ -0,0 +1,297 @@
+﻿using NUnit.Framework;
+
+namespace DemoQa.Tests
+{
+    public class SampleTest : BaseTest
+    {
+        [Test]
+        public void HomePage()
+        {
+            Scroll(150, 0);
+            homePage.ElementsHomePage();
+            homePage.ElementTextBox();
+            Assert.AreEqual("Text Boxx", homePage.GetHeader().ToString());
+
+           /* textBox.FullName("Uros Beric");
+            textBox.Email("uros.beric@seavus.com");
+            textBox.CurrentAddress("Beograd");
+            textBox.PermanentAddress("Beograd");
+            Scroll(300, 0);
+            textBox.SubmitButton();
+            textBox.ElementCheckBox();
+
+            checkBox.ToggleHome();
+            checkBox.ToggleDownloads();
+            checkBox.CheckBoxDownloads();
+            checkBox.CollapseAll();
+            checkBox.ElementRadioBtn();
+
+            radioButton.PressYesButton();
+            radioButton.ElementWebTables();
+            Scroll(300, 0);
+
+            webtables.AddNewRecordButton();
+            webtables.FirstName("Uros");
+            webtables.LastName("Beric");
+            webtables.Email("uros.beric@seavus.com");
+            webtables.Age("25");
+            webtables.Salary("800");
+            webtables.Department("Banking");
+            webtables.SubmitBtn();
+            webtables.DeleteRecord();
+            webtables.SearchBox("Uros");
+            webtables.EditRecord();
+            webtables.ClearSalary();
+            webtables.EditSalary("1100");
+            webtables.SubmitBtnEdit();
+            Scroll(100, 0);
+            webtables.ElementButtons();
+
+            Scroll(0, 0);
+            buttons.DoubleClick();
+            buttons.RightClick();
+            buttons.ClickBtn();
+            Scroll(200, 0);
+            buttons.ElementLinks();
+
+            Scroll(0, 0);
+            links.HomeLink();
+            SwitchTab(0);
+            links.HomeLink2();
+            SwitchTab(0);
+            Scroll(150, 0);
+            links.Created();
+            links.NoContent();
+            links.Moved();
+            links.BadRequest();
+            links.Unauthorized();
+            links.Forbidden();
+            links.NotFound();
+            Scroll(250, 0);
+            links.ElementBrokenLinks();
+
+            Scroll(100, 0);
+            brokenLinks.ValidLink();
+            GoBack();
+            Scroll(100, 0);
+            brokenLinks.BrokenLink(); //chrome
+            GoBack();
+            Scroll(300, 0);
+            brokenLinks.ElementUploadAndDownload();
+
+            //uploadDownload.DownloadBtn();  //download file
+            uploadDownload.UploadBtn(@"C:\Users\Uros.Beric\Desktop\C#\DemoQa\DemoQa\File\Screenshot_1.png");
+            Scroll(400, 0);
+            uploadDownload.ElementDynamicProperties();
+
+            CloseAds();
+            HideElements();
+
+            dynamicProperties.EnableBtn();
+            dynamicProperties.ChangeColorBtn();
+            dynamicProperties.VisibleAfterBtn();
+            Scroll(550, 0);
+            dynamicProperties.Forms();
+            dynamicProperties.PracticeFormsForm();
+
+            Scroll(50, 0);
+            practiceForm.FirstName("Uros");
+            practiceForm.LastName("Beric");
+            practiceForm.Email("uros.beric@seavus.com");
+            practiceForm.PressMaleButton();
+            practiceForm.MobileNumber("0620640752");
+            Scroll(10, 0);
+            practiceForm.DateOfBirth();
+            practiceForm.YearOfBirth();
+            practiceForm.PickYear();
+            practiceForm.MonthOfBirth();
+            practiceForm.PickMonth();
+            practiceForm.PickDay();
+            Scroll(200, 0);
+            practiceForm.Hobbies();
+            practiceForm.SubmitBtn();
+            practiceForm.CloseBtn();
+
+
+
+            practiceForm.AlertsFrameWindows();
+            practiceForm.AlertsBrowserWindows();
+
+            browserWindows.NewTab();
+            SwitchTab(0);
+            browserWindows.NewWindow();
+            SwitchTab(0);
+            browserWindows.NewWindowMessage();
+            SwitchTab(0);
+            Scroll(200, 0);
+            browserWindows.Alerts();
+
+            alerts.Alert();
+            PressAlert();
+            alerts.AlertWait();
+            PressAlert();
+            alerts.ComfirmAlert();
+            PressAlert();
+            alerts.PromptAlert();
+            TextAlert("Uros");
+            PressAlert();
+            Scroll(300, 0);
+            alerts.AlertFrames();
+            Scroll(200, 0);
+            alerts.AlertNestedFrames();
+            Scroll(200, 0);
+            alerts.AlertModealDialogs();
+
+
+            modalDialogs.SmallModal();
+            modalDialogs.CloseSmall();
+            modalDialogs.LargeModal();  //chrome
+            modalDialogs.CloseLarge();
+            Scroll(300, 0);
+            modalDialogs.Widgets();
+            Scroll(300, 0);
+            modalDialogs.WidgetsAccordian();
+
+            accordian.Section2();
+            Scroll(200, 0);
+            accordian.Section3();
+            accordian.WidgetsAutoComplete();
+            Scroll(150, 0);
+
+            //autoComplete.MultipleColorClick();
+            // autoComplete.MultipleColor("y");
+            // autoComplete.MultipleColorEnter();
+            autoComplete.WidgetsDatePicker();
+
+            datePicker.SelectDate();
+            datePicker.SelectYear();
+            datePicker.PickYear();
+            datePicker.SelectMonth();
+            datePicker.PickMonth();
+            datePicker.PickDay();
+            datePicker.DateAndTimePickerInput();
+            datePicker.SelectYearDateTime();
+            datePicker.PickYearDateTime();
+            datePicker.PickTIme();
+            Scroll(300, 0);
+            datePicker.WidgetsSlider();
+
+            slider.ClickSlider();
+            slider.MoveSlider(39);
+            Scroll(300, 0);
+            slider.WidgetsProgressBar();
+
+            progressBar.StartProgressBar();
+            progressBar.StopProgressBar();
+            Scroll(350, 0);
+            progressBar.WidgetsTabs();
+
+            tabs.OriginTab();
+            tabs.UseTab();
+            tabs.WhatTab();
+            Scroll(450, 0);
+            tabs.WidgetsToolTip();
+
+            toolTips.HoverButton();
+            toolTips.HoverTextField();
+            toolTips.HoverText();
+            Scroll(550, 0);
+            toolTips.WidgetsMenu();
+
+            menu.MainItem2(); //fireFox chrome
+            menu.SubList();
+            menu.SubItem2();
+            Scroll(750, 0);
+            menu.WidgetsSelectMeny();
+
+
+            selectMenu.SelectOption();
+            selectMenu.PickOptions();
+            selectMenu.SelectOne();
+            selectMenu.PickOne();
+            selectMenu.Multiselect();
+            selectMenu.PickMultiSelect();
+            selectMenu.StandardMultiSelect();
+            Scroll(-200, 0);
+            selectMenu.CloseWidgets();
+            Scroll(250, 0);
+            selectMenu.OpenInteractions(); //chrome
+            selectMenu.InteractionsSortable();
+
+            sortable.DragSixOnOne();
+            sortable.DragTwoOnFour();
+            sortable.Grid();
+            sortable.DragThreeToSeven();
+            Scroll(500, 0);
+            sortable.InteractionsSelectable();
+
+            selectable.ListFour();
+            selectable.ListTwo();
+            selectable.Grid();
+            selectable.GridFive();
+            selectable.GridNine();
+            Scroll(500, 0);
+            selectable.InteractionsResizable();
+
+            resizable.ResizableBox(300, 250);  //fireFox
+            Scroll(300, 0);
+            resizable.ResizableBoxBig(500, 100);
+            Scroll(100, 0);
+            resizable.InteractionsDroppable();
+
+
+            droppable.SimpleDropBox(); //fireFox
+            droppable.AcceptTab();
+            droppable.AcceptableDropBox();
+            droppable.PreventTab();
+            droppable.PreventDropBoxOuter();
+            Scroll(200, 0);
+            droppable.PreventDropBoxOuterDrop();
+            droppable.RevertTab();
+            droppable.WillRevert();
+            droppable.NotRevert();
+            Scroll(500, 0);
+            droppable.InteractionsDragabble();
+
+            dragabble.SimpleDropBox(320, 150);  //fireFox
+            dragabble.AxisTab();
+            dragabble.XDrag(200);
+            dragabble.YDrag(100);
+            dragabble.ContainerTab();
+            dragabble.ContainerBox(500, 200);
+            Scroll(100, 0);
+            dragabble.ContainerParent(20, 20);
+            Scroll(500, 0);
+            dragabble.BookStore();  //chrome
+            dragabble.LoginBS();
+            Scroll(-500, 0);
+
+            login.NewUserBtn();
+
+
+            /* register.FirstName("Uros");
+              register.LastName("Beric");
+              register.UserName("Bera2");
+              register.Password("Ratimir233@");
+              register.Captcha();
+              DefaultFrame();
+              register.RegisterBtn();
+              PressAlert();
+              
+            register.GoToLoginBtn();
+            login.UserName("Bera2");
+            login.Password("Ratimir233@");
+            login.LoginBtn();
+
+            profile.GoToStore();
+
+            bookStore.PickBook();
+            Scroll(300, 0);
+            bookStore.AddToCollection();
+            PressAlert();
+            bookStore.GoBack();
+            Scroll(300, 0);
+            bookStore.Profil();*/
+        }
+    }
+}
