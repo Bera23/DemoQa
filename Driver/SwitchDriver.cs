f40f68ea74af3f567ffcc2ad802cb403913b471f
Uros Beric <47391101+Bera23@users.noreply.github.com>
2022-04-08 13:39:28 +0200

    Add files via upload

---
+++ Driver/SwitchDriver.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+
+namespace DemoQa.Driver
+{
+    public class SwitchDriver
+    {
+        public WebDriver driver;
+        public void Switch(string browser)
+        {
+            {
+                switch (browser)
+                {
+                    case "chrome":
+                        driver = new ChromeDriver();
+                        break;
+                    case "fireFox":
+                        driver = new FirefoxDriver();
+                        break;
+                    case "edge":
+                        driver = new EdgeDriver();
+                        break;
+                    case "internetExplorer":
+                        driver = new InternetExplorerDriver();
+                        break;
+                }
+
+            }
+        }
+    }
+}
